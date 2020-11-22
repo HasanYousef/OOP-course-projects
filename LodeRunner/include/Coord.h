e60216709e6a1443fff89f0d6a9bce535bb0703e
HasanYousef <hasan.yousef.cs@gmail.com>
2020-11-22 18:53:47 +0200

    improvments

--- LodeRunner/include/Coord.h
+++ LodeRunner/include/Coord.h
@@ -4,3 +4,19 @@ struct Coord {
 	int m_col,
 		m_row;
 };
+
+Coord &Coord::operator=(const Coord &other) {
+	if (&other != this) {
+		m_col = other.m_col;
+		m_row = other.m_row;
+	}
+	return *this;
+}
+
+bool operator==(const Coord& l, const Coord& r) {
+	return (l.m_col == r.m_col && l.m_row == r.m_row);
+}
+
+bool operator!=(const Coord& l, const Coord& r) {
+	return !(l == r);
+}
